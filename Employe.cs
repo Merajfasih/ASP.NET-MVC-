e6ea784bf7c93a396fedb8ac1faf8603b73a9e7f
Meraj Fasih <56186101+Merajfasih@users.noreply.github.com>
2020-10-10 11:21:39 -0700

    Add files via upload

---
+++ Employe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTMLhelper.Models
+{
+    public class Employe
+    {
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        public bool IsEmoloye { get; set; }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HTMLhelper.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name =  "Please enter your name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsOnline { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Dateofbirth { get; set; }
+    }
+}
